e221c52ada8eb1ad533f2ab9725b1dbac5cb6991
taoli2016 <871902595@qq.com>
2017-04-23 20:13:24 +0800

    Initial commit

---
+++ app/data.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+var username = [{
+    id : 1,
+    name : '陶大力',
+    work : '设计师，博客',
+    img :"images/f1.jpg",
+},{
+	id : 2,
+    name : '陶二力',
+    work : '作家，杂志编辑',
+    img :"images/f2.jpg",
+},{
+    id : 3,
+    name : '陶三力',
+    work : '艺术总监，电影剪辑',
+    img :"images/f3.jpg",
+},{
+    id : 4,
+    name : '陶小力',
+    work : '音乐家，运动员',
+    img :"images/f4.jpg",
+}];
